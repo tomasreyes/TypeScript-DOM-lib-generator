ea72cc9ab9d22b99060184d57835313b61ddd562
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-25 15:22:53 -0700

    Update migrate script for new lib dir (#2531)

--- deploy/migrate.js
+++ deploy/migrate.js
@@ -1,6 +1,5 @@
 // @ts-check
-// Mainly a quick script to migrate the generated files into the
-// lib folder of a TypeScript clone.
+// Mainly a quick script to migrate the generated files into a TypeScript clone.
 //
 // node ./deploy/migrate.js [optional/file/path/to/tsc]
 
@@ -16,6 +15,11 @@ if (!tscWD) {
   );
 }
 
+const libDir = join(tscWD, "tsc", "internal", "bundled", "libs");
+const copyrightNotice = readFileSync(
+  join(tscWD, "tsc", "internal", "bundled", "CopyrightNotice.txt"),
+  "utf8",
+);
 const generatedFiles = readdirSync("generated");
 const filesToSend = generatedFiles.filter(
   (file) => file.includes("dom.") || file.includes("webworker."),
@@ -23,12 +27,13 @@ const filesToSend = generatedFiles.filter(
 
 filesToSend.forEach((file) => {
   const contents = readFileSync(join("generated", file), "utf8");
-  const newFilePath = join(tscWD, "src", "lib", file);
-  writeFileSync(newFilePath, contents);
+  const target = `lib.${file.replace(".generated", "")}`;
+  const newFilePath = join(libDir, target);
+  writeFileSync(newFilePath, `${copyrightNotice}\n${contents}`);
 });
 
 console.log(
   `Moved ${filesToSend
-    .map((f) => f.replace(".generated", ""))
-    .join(", ")} to '${tscWD}/src/lib'.`,
+    .map((file) => `lib.${file.replace(".generated", "")}`)
+    .join(", ")} to '${libDir}'.`,
 );
